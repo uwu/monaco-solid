68142f0f0a712e99dcf85f8cc17cc6ce23ece340
yellowsink <yellowsink@riseup.net>
2022-06-22 11:03:59 +0000

    create test app

---
+++ test-app/src/index.tsx
@@ -0,0 +1,18 @@
+/* @refresh reload */
+import { createSignal } from "solid-js";
+import { render } from "solid-js/web";
+import Monaco from "../..";
+
+render(() => {
+  const [val, setVal] = createSignal("");
+
+  return (
+    <>
+      <h1>Monaco solid test</h1>
+      <Monaco value={val()} lang="js" valOut={setVal} />
+      <pre>
+        <code>{val()}</code>
+      </pre>
+    </>
+  );
+}, document.querySelector("body") as HTMLElement);
