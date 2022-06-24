087c319df8e14a90a52053f0d4a97124ab158730
yellowsink <yellowsink@riseup.net>
2022-06-24 12:00:37 +0100

    fix duplicate theme requests

--- src/monaco.ts
+++ src/monaco.ts
@@ -10,12 +10,13 @@ export const addThemeIfNeeded = async (t: string) => {
 	if (!t || !t.trim()) return;
 	if (loadedThemes.has(t)) return;
 
+	loadedThemes.add(t);
+
 	const u = `https://cdn.esm.sh/monaco-themes@0.4.2/themes/${t}.json`;
 
 	const theme = await fetch(u).then((r) => r.json());
 
 	monaco.editor.defineTheme(t, theme);
-	loadedThemes.add(t);
 };
 
 export const initMonacoIfNeeded = async () => {
